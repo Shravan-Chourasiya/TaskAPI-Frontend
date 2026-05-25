797532b38c9f04c226287293604e03705ef1b67a
Shravan-Chourasiya <shravan.r.chaursiya@gmail.com>
2026-05-25 19:49:15 +0530

    Add Toaster component to main render; define TOAST_AUTO_CLOSE_DURATION constant

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const USE_IS_VERIFIED_USER_INTERVAL = 10 * 60 * 1000; // 10 minutes in mi
 export const TIME_TO_WAIT_BEFORE_EXPIRY = 60 * 1000; // 1 minute in milliseconds
 export const REFRESH_TIMEOUT_MS = 10_000;
 export const TOKEN_ERROR_STATUSES = new Set([401, 403]);
+export const TOAST_AUTO_CLOSE_DURATION = 4000; // 4 seconds
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <App />
+    <>
+      <App />
+      <Toaster position="top-right" closeButton duration={4000} />
+    </>
   // </StrictMode>
 )
